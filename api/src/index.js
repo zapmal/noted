42c978ac20afdd19e15a0d8330e30bdbd353a829
Zondazx <mzxgd16@gmail.com>
2020-12-17 21:21:55 -0400

    feat: initial graphql tests

--- api/src/index.js
+++ api/src/index.js
@@ -4,15 +4,60 @@ const {
   gql
 } = require('apollo-server-express');
 
+let notes = [
+  {
+    id: '1',
+    content: 'This is a note',
+    author: 'Adam Scott'
+  },
+  {
+    id: '2',
+    content: 'This is another note',
+    author: 'Harlow Everly'
+  },
+  {
+    id: '3',
+    content: 'Oh hey look, another note!',
+    author: 'Riley Harrison'
+  }
+];
+
 const typeDefs = gql`
+  type Note {
+    id: ID
+    content: String
+    author: String
+  }
+
   type Query {
     hello: String
+    notes: [Note]
+    note(id: ID): Note
+  }
+
+  type Mutation {
+    newNote(content: String!): Note
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      const noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Zondazx'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
   }
 };
 
@@ -29,7 +74,9 @@ server.applyMiddleware({
   path: '/api'
 });
 
-app.listen({ port }, () =>
+app.listen({
+    port
+  }, () =>
   console.log(
     `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
   )
